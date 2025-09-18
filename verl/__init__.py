ba562cf9ca68e358f9290b4a456e6bfc0bb852d6
Yaowei Zheng <hiyouga@buaa.edu.cn>
2025-09-18 17:28:41 +0800

    [misc] set dev version (#503)

--- verl/__init__.py
+++ verl/__init__.py
@@ -21,7 +21,7 @@ if is_package_available("modelscope"):
     from modelscope.utils.hf_util import patch_hub  # type: ignore
 
 
-__version__ = "0.3.2"
+__version__ = "0.3.3.dev0"
 
 
 if os.getenv("USE_MODELSCOPE_HUB", "0").lower() in ["true", "y", "1"]:
