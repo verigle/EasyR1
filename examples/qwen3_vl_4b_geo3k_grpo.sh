f37f82abe3da10f262694ebcadaf806b13a1cebe
Yaowei Zheng <hiyouga@buaa.edu.cn>
2025-10-15 13:38:03 +0800

    [examples] add qwen3vl 4b model (#533)

--- examples/qwen3_vl_4b_geo3k_grpo.sh
+++ examples/qwen3_vl_4b_geo3k_grpo.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
+MODEL_PATH=Qwen/Qwen3-VL-4B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
     data.train_files=hiyouga/geometry3k@train \
     data.val_files=hiyouga/geometry3k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
-    trainer.experiment_name=qwen2_5_vl_3b_geo_grpo \
+    trainer.experiment_name=qwen3_vl_4b_geo_grpo \
     trainer.n_gpus_per_node=2
